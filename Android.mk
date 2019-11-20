164050d3d94bbac6dc11a3448a78939d8b953f53
ripee <ripee.dev@gmail.com>
2019-11-20 09:58:16 +0100

    zero-common: Revise asserts

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
-ifneq ($(filter zerofltedv zerofltemtr zerofltespr zerofltetmo zerofltexx zeroltetmo zeroltexx zerofltebmc zeroltebmc zeroltecan zerofltecan,$(TARGET_DEVICE)),)
+ifneq ($(filter zeroflte zerofltebmc zerofltecan zerofltedv zerofltemtr zerofltespr zerofltetmo zerofltexx zerolte zeroltebmc zeroltecan zeroltedv zeroltemtr zeroltespr zeroltetmo zeroltexx,$(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
