88d18ed204c15f5fb3e644f65a1f0be4f1440f49
ripee <ripee.dev@gmail.com>
2019-12-10 22:41:50 +0100

    zero-common: Rename libnfc-brcm.conf to libnfc-nci.conf

--- zero-common.mk
+++ zero-common.mk
@@ -242,9 +242,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:system/vendor/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/libnfc-sec-hal.conf:system/vendor/etc/libnfc-sec-hal.conf \
-    $(LOCAL_PATH)/configs/libnfc-sec.conf:system/vendor/etc/libnfc-sec.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/vendor/etc/libnfc-nci.conf
+    $(LOCAL_PATH)/configs/libnfc-sec.conf:system/vendor/etc/libnfc-sec.conf
 
 # Network
 PRODUCT_PACKAGES += \
