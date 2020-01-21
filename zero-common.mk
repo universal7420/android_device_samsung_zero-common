4895f97967e7d7dd2eb29efcc1a424cd83b3b7f2
enesuzun2002 <enesuzun200227@gmail.com>
2020-01-21 05:33:27 +0000

    zero: drop custom BT hidl-impl

--- zero-common.mk
+++ zero-common.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl.zero \
+	android.hardware.bluetooth@1.0-impl \
 	android.hardware.bluetooth@1.0-service \
 	libbt-vendor
 
